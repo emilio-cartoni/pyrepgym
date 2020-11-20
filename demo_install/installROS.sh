6e3af07d05d88cb23473bc56128b706d39ac7914
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-11-20 17:03:00 +0100

    Update installROS.sh

--- demo_install/installROS.sh
+++ demo_install/installROS.sh
@@ -4,7 +4,7 @@ set -e
 
 cd
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install -y ros-kinetic-desktop-full
